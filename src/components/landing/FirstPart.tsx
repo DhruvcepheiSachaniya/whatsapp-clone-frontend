6780482c58ab38d05b95817c6c417e171f20b698
Dhruv <sachaniyadhruv@email.com>
2024-12-18 22:00:31 +0530

    Start working on Landing

---
+++ src/components/landing/FirstPart.tsx
@@ -0,0 +1,11 @@
+import { Box, Typography } from "@mui/material";
+
+const FirstPart: React.FC = () => {
+  return (
+    <Box>
+      <Typography variant="h1">First Part</Typography>
+    </Box>
+  );
+};
+
+export default FirstPart;
